3eb02ccf20af7f1a72ee0b981ea522ceae3d11f0
Mosallam Abdulrahman <mosallamabdulrahman@gmail.com>
2026-02-01 09:03:39 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-
 module.exports = {
-  content: ["./about.html"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
       colors: {
